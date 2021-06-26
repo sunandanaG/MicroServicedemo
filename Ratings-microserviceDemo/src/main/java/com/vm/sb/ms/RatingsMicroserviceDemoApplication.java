731e3096a93b6b1b3e7a22b909c4e1e915c0203c
Doresh Ghanta <Doresh Ghanta@Doresh-Desktop>
2021-06-26 18:20:11 +0530

    Ratings

---
+++ Ratings-microserviceDemo/src/main/java/com/vm/sb/ms/RatingsMicroserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vm.sb.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsMicroserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsMicroserviceDemoApplication.class, args);
+	}
+
+}
