7c72baafe1984edfb26c64e086cd40c3c9ae264e
Doresh Ghanta <Doresh Ghanta@Doresh-Desktop>
2021-06-26 18:18:44 +0530

    MovieInfo demo

---
+++ MovieInfo-microserviceDemo-/src/main/java/com/vm/sb/ms/MovieResource.java
@@ -0,0 +1,28 @@
+package com.vm.sb.ms;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+import com.vm.sb.ms.model.Movie;
+
+/**
+ * this microservice returns the movie details of each movie id provided to it
+ * @author Admin
+ *
+ */
+
+@RestController
+	@RequestMapping("/movies")
+	public class MovieResource {
+		
+		@RequestMapping("/{movieId}")
+		public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+			return new Movie("movieid1", "moviename1");
+		}
+
+}
